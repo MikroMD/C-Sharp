c0a3c37cb4db94fc5f49f6892e8917b55a8c5e8a
MikroMD <95480335+MikroMD@users.noreply.github.com>
2022-01-01 12:37:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string to Check Palindrome : ");
+            string str = Console.ReadLine();
+
+            if (str.Length > 0)
+            {
+                char[] test = str.ToCharArray();
+                Array.Reverse(test);
+                string reverse = new string(test);
+
+                if (str.Equals(reverse, StringComparison.OrdinalIgnoreCase))
+                    Console.WriteLine($"{str} is Palindrome.");
+                else
+                    Console.WriteLine($"{str} is not Palindrome.");
+            }
+            else
+                Console.WriteLine($"{str} is empty.");
+        }
+    }
+}
